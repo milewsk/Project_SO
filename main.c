befbf246c98f0d3ba37b39545ae20b9ea7914f81
Konrad Milewski <59485035+milewsk@users.noreply.github.com>
2021-04-07 22:06:37 +0200

    Zmiany
    +Dodanie maina
    +dodanie getopt (obsługa przekazanych parametrów)

--- main.c
+++ main.c
@@ -4,7 +4,27 @@
 #define X 5
 // main 
 
-//test
+int main(int argc, char* argv[])
+{
+
+
+
+	// obsługa wprowadzonych wartości z terminalu
+	//s - sleep = czas spania 
+	//i	= scieżka początkowy folder (in)
+	//o  = scieżka folder docelowy (out)
+	//r = rozmiar
+
+	//pętla wykonująca się w kółko gdy nasz programik działa
+	while ((getotp(argc,argv, s:i:o:r:)) != -1)
+	{
+
+
+	}
+
+
+
+}
 
 
 // raz dwa trzy
